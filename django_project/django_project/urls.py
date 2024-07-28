73875af14b6858dfc855f8bf418f0635be08226e
Rachit74 <rachithooda09@gmail.com>
2024-07-28 20:00:11 +0530

    basic run

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('blog/', include("blog.urls")), #mapping blog urls to get routes of blog app in project
     path('admin/', admin.site.urls),
 ]
