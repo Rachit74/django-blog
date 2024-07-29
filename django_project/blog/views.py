119223780c19455399fee07769e0c2a4a8cceb68
Rachit74 <rachithooda09@gmail.com>
2024-07-29 18:06:28 +0530

    database

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,37 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-# dummy data
-
-posts = [
-    {
-        'id': 1,
-        'title': 'Blog1',
-        'author': 'User1',
-        'content': 'Hello There!',
-        'date': '1st june, 1936, 19:45',
-    },
-    {
-        'id': 2,
-        'title': 'Blog2',
-        'author': 'User2',
-        'content': 'Hello There!',
-        'date': '4 june, 2024, 13:43',
-    },
-    {
-        'id': 3,
-        'title': 'Blog3',
-        'author': 'User3',
-        'content': 'Hello There!',
-        'date': '16 Decenber, 1971, 20:00',
-    },
-]
+from . models import Post
 
 #views
 
 def home(request):
     context = {
-        'posts': posts,
+        'posts': Post.objects.all(),
     }
     return render(request,'blog/home.html', context=context)
 
