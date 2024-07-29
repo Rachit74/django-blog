c639c89072a2a66c4e028cfca409535bc6b595e9
Rachit74 <rachithooda09@gmail.com>
2024-07-29 16:11:05 +0530

    dummy data and CSS Styling

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# dummy data
+
+posts = [
+    {
+        'id': 1,
+        'name': 'Rachit',
+        'content': 'Hello There!',
+    },
+    {
+        'id': 2,
+        'name': 'TestUser1',
+        'content': 'Hello',
+    },
+    {
+        'id': 3,
+        'name': 'TestUser2',
+        'content': 'There Hello',
+    },
+]
+
 #views
 
 def home(request):
-    return render(request,'blog/home.html')
+    context = {
+        'posts': posts,
+    }
+    return render(request,'blog/home.html', context=context)
 
 def about(request):
-    return HttpResponse("Blog About")
+    return render(request, "blog/about.html")
