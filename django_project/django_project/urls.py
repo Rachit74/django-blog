f4be0cc969fa7709fd3179a2d3d8bed0c5129fe0
Rachit74 <rachithooda09@gmail.com>
2024-07-30 05:57:17 +0530

    user login and logout

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -17,9 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
+from django.contrib.auth import views as auth_views #To handle login and logout
 
 urlpatterns = [
     path('', include("blog.urls")), #mapping blog urls to get routes of blog app in project
     path('register/', user_views.register, name='register'),
+    #login and logut views
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('admin/', admin.site.urls),
 ]
